183ddd961e243ddfd641383b29624b8e68d0de03
stephen ligtenberg <bountyhunter24@gmail.com>
2023-04-01 21:02:41 -0600

    commit tree a68b2b870b0fcc52154db06e177198e57f1dae8e

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # find a cheese using the ID from the URL
+    # send a json responce using that cheese object
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
+
 end
